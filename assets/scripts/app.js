b4df953425cf873f1f12857837c90e93b7f7e6c4
Aditya Soni <soniadityakumar651@gmail.com>
2023-09-11 13:20:27 +0530

    Initial file edit

---
+++ assets/scripts/app.js
@@ -0,0 +1,86 @@
+// // // // // // // import { apikey } from "./util";
+
+// // // // // // import apikey from "./util";
+// // // // // // console.log(apikey);
+
+// // // // // let userMessage = "Hello World!!";
+
+// // // // // console.log(userMessage); 
+
+// // // // console.log(23 + 42);
+
+// // // // console.log("Hello" + "World");
+
+
+// // // // console.log(10 === 5); // tirple wqual sign is use for equality for boolean true or false value.
+
+// // // // if (10 === 10)
+// // // // {
+// // // //   console.log("works.....");
+// // // // }
+
+
+// // // // `  `                                                 FUNCTIONS =>
+
+// // // function greet() {
+// // //   // console.log("Hello Everyone!!");
+ 
+// // //   return "Hi, i am" + greet +"hello";
+
+// // // }
+// // // greet(); // calling of the function.
+
+// // // function grretUser(userName, userMessage) {
+// // //   console.log(userMessage);
+// // //   console.log(userName);
+// // // }
+// // // grretUser("Aditya", "How are you ?");
+// // // grretUser("Raj", "What up my g ?")
+
+// // // // The above two are the types of writting a function in the javascript.
+
+// // // export default (userName, Message) => {
+// // //   console.log("Hello");
+// // //   return userName, Message;
+// // // }
+
+
+// // //                                                  Objects and Classes
+
+// // const userName = "Aditya";
+// // const userAge = "20";
+
+
+// // const user = {
+// //   name: "Adit",
+// //   age: 20,
+// //   great(){
+// //     return "Hello World!"; // This is the main function.
+// //     console.log(this.age); // It will just give the age.
+// //   }
+
+// // };
+
+// // console.log(user); // both outputs .
+// // console.log(useer.name);   // Only Aditya as the output.
+
+// class user {
+//   constructor(name,age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greet() {
+//     console.log("HI!!");
+//   }
+// }
+
+// const user1 = new user("Raj", 34); // Object will be 
+
+// console.log(user1);
+
+
+// user1.greet();
+
+
+//                                                              ARRAYS
+
